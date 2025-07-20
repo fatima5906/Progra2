5988f6a21c788908ffb3615d2624b6899b577d7a
fatima5906 <fgaitang1@miumg.edu.gt>
2025-07-20 16:44:29 -0600

    Clase agregada

--- lab1/src/Laboratorio.java
+++ lab1/src/Laboratorio.java
@@ -1,6 +1,5 @@
 import java.util.LinkedList;
 import java.util.Queue;
-
 public class Laboratorio {
     public static void main (String [] args){
         Queue<String> pedidos = new LinkedList<>();
